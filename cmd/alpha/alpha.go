664d4b9c94d02488b8f7b0989cb175b07aeb2179
Ashish Amarnath <ashish.amarnath@gmail.com>
2019-08-22 11:31:32 -0700

    generate core cluster yaml

--- cmd/alpha/alpha.go
+++ cmd/alpha/alpha.go
@@ -16,7 +16,12 @@ limitations under the License.
 
 package alpha
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/ashish-amarnath/capiyaml/cmd/alpha/capi"
+	"github.com/spf13/cobra"
+)
 
 // Cmd returns the set alpha of commands
 func Cmd() *cobra.Command {
@@ -28,6 +33,30 @@ func Cmd() *cobra.Command {
 			cmd.Help()
 		},
 	}
-
+	alphaCmd.AddCommand(getClusterYamlCommand())
 	return alphaCmd
+}
+
+func getClusterYamlCommand() *cobra.Command {
+	var provider, clusterName, clusterNamespace string
+
+	cmd := &cobra.Command{
+		Use:   "get-cluster-yaml",
+		Short: "generate yaml for cluster",
+		Long:  "generate yaml for cluster",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Genarating yaml for cluster %q for infrastructure provider %q\n", clusterName, provider)
+			coreClusterYaml, err := capi.GetCoreClusterYaml(clusterName, clusterNamespace, provider)
+			if err != nil {
+				fmt.Printf("%v\n", err)
+			}
+			fmt.Printf("%s\n", coreClusterYaml)
+		},
+	}
+
+	cmd.Flags().StringVarP(&provider, "provider", "p", "", "Infrastructure provider for which yaml needs to be generated")
+	cmd.Flags().StringVarP(&clusterName, "name", "n", "", "Name for the cluster")
+	cmd.Flags().StringVarP(&clusterNamespace, "namespace", "", "default", "Namespace where the cluster will be created")
+
+	return cmd
 }
--- cmd/alpha/capi/capi.go
+++ cmd/alpha/capi/capi.go
@@ -15,3 +15,45 @@ limitations under the License.
 */
 
 package capi
+
+import (
+	"fmt"
+
+	"encoding/json"
+
+	v1 "k8s.io/api/core/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha2"
+)
+
+const (
+	coreAPIVersion  = "cluster.x-k8s.io/v1alpha2"
+	infraAPIVersion = "infrastructure.cluster.x-k8s.io/v1alpha2"
+
+	prettyJSONPrefix = ""
+	prettyJSONIndent = "	" // tab
+)
+
+// GetCoreClusterYaml returns yaml for CAPI  cluster objects
+func GetCoreClusterYaml(name, namespace, infraProvider string) (string, error) {
+	coreCluster := clusterv1.Cluster{}
+	coreCluster.Name = name
+	coreCluster.Namespace = namespace
+	coreCluster.APIVersion = coreAPIVersion
+
+	coreCluster.Spec = clusterv1.ClusterSpec{
+		InfrastructureRef: &v1.ObjectReference{
+			Kind:       fmt.Sprintf("%sCluster", infraProvider),
+			APIVersion: infraAPIVersion,
+			Name:       name,
+			Namespace:  namespace,
+		},
+	}
+
+	rawBytes, err := json.MarshalIndent(coreCluster, prettyJSONPrefix, prettyJSONIndent)
+	if err != nil {
+		fmt.Printf("Failed to generate yaml for coreCluster %q, %v", name, err)
+		return "", err
+	}
+
+	return string(rawBytes), nil
+}
